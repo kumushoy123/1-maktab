cd2fb4bb1f82e0e8830d7e8030176eea6cbf7fba
kumushoy123 <rahmonqulovakumushoy8@gmail.com>
2026-02-27 00:20:02 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,14 +13,26 @@ from sinf import sinf_soni
 # ENV YUKLASH
 # ======================
 load_dotenv()
-TOKEN = getenv("BOT_TOKEN")
-ADMIN_ID = int(getenv("ADMIN_ID"))  # Telegram ID
+
+# BOT TOKEN
+BOT_TOKEN = getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN .env faylida topilmadi! Iltimos, to‘g‘ri yozing.")
+
+# ADMIN ID
+ADMIN_ID = getenv("ADMIN_ID")
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID .env faylida topilmadi! Iltimos, to‘g‘ri yozing.")
+try:
+    ADMIN_ID = int(ADMIN_ID)
+except ValueError:
+    raise ValueError("ADMIN_ID noto‘g‘ri yozilgan! Faqat raqam bo‘lishi kerak.")
 
 dp = Dispatcher()
-bot = Bot(token=TOKEN)
+bot = Bot(token=BOT_TOKEN)
 
 
-# ⭐ /start
+# /start
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer(
@@ -29,7 +41,7 @@ async def start(message: Message):
     )
 
 
-# ⭐ Maktab haqida
+# Maktab haqida
 @dp.message(F.text == "1-maktab haqida🏨")
 async def about(msg: Message):
     await msg.answer(
@@ -37,19 +49,19 @@ async def about(msg: Message):
     )
 
 
-# ⭐ Fanlar
+# Fanlar
 @dp.message(F.text == "📚Fanlar")
 async def courses(msg: Message):
     await msg.answer("Maktab darsliklari📖:", reply_markup=fanlar)
 
 
-# ⭐ Sinflar
+# Sinflar
 @dp.message(F.text == "Sinflar")
 async def classes(msg: Message):
     await msg.answer("Maktabimiz sinflari:", reply_markup=sinflar)
 
 
-# ⭐ SINFLAR SONI
+# Sinflar o‘quvchi soni
 @dp.message(F.text.in_(sinf_soni.keys()))
 async def sinf_soni_chiqar(msg: Message):
     sinf = msg.text
@@ -57,21 +69,21 @@ async def sinf_soni_chiqar(msg: Message):
     await msg.answer(f"📚 {sinf} sinfida {soni} ta o‘quvchi bor")
 
 
-# ⭐ Orqaga
+# Orqaga
 @dp.message(F.text == "🔙orqaga")
 async def back(msg: Message):
     await msg.answer("Asosiy menyu:", reply_markup=menu)
 
 
-# ⭐ Manzil
+# Manzil
 @dp.message(F.text == "📍Manzilimiz")
 async def location(msg: Message):
     await msg.answer("Bizning manzilimiz:")
-    await msg.answer("Sirdaryo viloyati, Mirzaobod tumani, Yangi hayot mahallasi")
+    await msg.answer("Sirdaryo viloyati, Mirzaobod tumani, 1-maktab")
 
 
 
-# ⭐ TELEFON QABUL QILISH (📞 NOMER QOLDIRISH)
+# Telefon qabul qilish
 @dp.message(F.contact)
 async def send_contact(msg: Message):
     phone = msg.contact.phone_number
@@ -88,10 +100,9 @@ async def send_contact(msg: Message):
     await msg.answer("Rahmat ❤️ Tez orada siz bilan bog‘lanamiz.")
 
 
-# ⭐ Botni ishga tushirish
+# Botni ishga tushirish
 async def main():
     print("Bot ishga tushdi...")
-    # ⭐ skip_updates=True bilan polling
     await dp.start_polling(bot, skip_updates=True)
 
 
